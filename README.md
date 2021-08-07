37ed818c94535749a3a30b628cb965ec207a5a88
lwebb-dev <logan@lwebb-dev>
2021-08-06 23:33:48 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# www.lwebb.dev
+
+This is my static portfolio site that will also be the home of the lwebb.dev domain for future projects that I may want to use it for (Example: lwebb.dev/project).
+
+This repository uses Node.js to harness the gulp.js cli for the build process. Make sure Node.js is installed as well as gulp-cli installed globally.
+Use `npm install` to pull the neccesary gulp plugins into node_modules.
+Use `npm run start` to start the gulp sass watcher, and `npm run publish` to build and minify the site from `src` into a `dist` directory.
---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const { watch, series, parallel } = require("gulp");
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const cleanCSS = require("gulp-clean-css");
+const htmlMin = require("gulp-htmlmin");
+
+function cssTranspile() {
+    return gulp.src("./src/scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./src/css"));
+};
+
+function cssPublish() {
+    return gulp.src("./src/css/**/*.css")
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("./dist/css"));
+}
+
+function htmlPublish() {
+    return gulp.src("./src/**/*.html")
+    .pipe(htmlMin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("./dist"));
+}
+
+exports.default = () => {
+    watch("./src/scss/*", cssTranspile);
+}
+
+exports.publish = series(cssTranspile, parallel(cssPublish, htmlPublish));
